311e88c8447fcb596f9e09990010ccb1c660bf4f
Adam Kelly <adam.guitar12@gmail.com>
2018-07-26 12:36:32 +0100

    Convert to ES5

--- index.js
+++ index.js
@@ -1,55 +1,68 @@
-'use strict';
-const parseMs = require('parse-ms');
+"use strict";
 
-const pluralize = (word, count) => count === 1 ? word : word + 's';
+var parseMs = require("parse-ms");
 
-module.exports = (ms, options = {}) => {
+var pluralize = function pluralize(word, count) {
+	return count === 1 ? word : word + "s";
+};
+
+module.exports = function(ms) {
+	var options =
+		arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
 	if (!Number.isFinite(ms)) {
-		throw new TypeError('Expected a finite number');
+		throw new TypeError("Expected a finite number");
 	}
 
 	if (ms < 1000) {
-		const msDecimalDigits = typeof options.msDecimalDigits === 'number' ? options.msDecimalDigits : 0;
-		return (msDecimalDigits ? ms.toFixed(msDecimalDigits) : Math.ceil(ms)) + (options.verbose ? ' ' + pluralize('millisecond', Math.ceil(ms)) : 'ms');
+		var msDecimalDigits =
+			typeof options.msDecimalDigits === "number" ? options.msDecimalDigits : 0;
+		return (
+			(msDecimalDigits ? ms.toFixed(msDecimalDigits) : Math.ceil(ms)) +
+			(options.verbose ? " " + pluralize("millisecond", Math.ceil(ms)) : "ms")
+		);
 	}
 
-	const ret = [];
+	var ret = [];
 
-	const add = (value, long, short, valueString) => {
+	var add = function add(value, long, short, valueString) {
 		if (value === 0) {
 			return;
 		}
 
-		const postfix = options.verbose ? ' ' + pluralize(long, value) : short;
+		var postfix = options.verbose ? " " + pluralize(long, value) : short;
 
 		ret.push((valueString || value) + postfix);
 	};
 
-	const secDecimalDigits = typeof options.secDecimalDigits === 'number' ? options.secDecimalDigits : 1;
+	var secDecimalDigits =
+		typeof options.secDecimalDigits === "number" ? options.secDecimalDigits : 1;
 
 	if (secDecimalDigits < 1) {
-		const diff = 1000 - (ms % 1000);
+		var diff = 1000 - (ms % 1000);
 		if (diff < 500) {
 			ms += diff;
 		}
 	}
 
-	const parsed = parseMs(ms);
+	var parsed = parseMs(ms);
 
-	add(Math.trunc(parsed.days / 365), 'year', 'y');
-	add(parsed.days % 365, 'day', 'd');
-	add(parsed.hours, 'hour', 'h');
-	add(parsed.minutes, 'minute', 'm');
+	add(Math.trunc(parsed.days / 365), "year", "y");
+	add(parsed.days % 365, "day", "d");
+	add(parsed.hours, "hour", "h");
+	add(parsed.minutes, "minute", "m");
 
 	if (options.compact) {
-		add(parsed.seconds, 'second', 's');
-		return '~' + ret[0];
+		add(parsed.seconds, "second", "s");
+		return "~" + ret[0];
 	}
 
-	const sec = ms / 1000 % 60;
-	const secFixed = sec.toFixed(secDecimalDigits);
-	const secStr = options.keepDecimalsOnWholeSeconds ? secFixed : secFixed.replace(/\.0+$/, '');
-	add(sec, 'second', 's', secStr);
+	var sec = (ms / 1000) % 60;
+	var secFixed = sec.toFixed(secDecimalDigits);
+	var secStr = options.keepDecimalsOnWholeSeconds
+		? secFixed
+		: secFixed.replace(/\.0+$/, "");
+	add(sec, "second", "s", secStr);
 
-	return ret.join(' ');
+	return ret.join(" ");
 };
